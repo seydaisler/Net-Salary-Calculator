6c8d1bdadb86136c5be2e56ae4a518a81a447b3e
seyda.isler <seydaisler@gmail.com>
2021-06-04 23:00:53 +0200

    fixing some issues

--- proje.py
+++ proje.py
@@ -17,16 +17,18 @@ def calculateChildTaxDiscount():
     return childCount * 0.5
 
 def calculateTaxCategory():
-    taxCategory=int(input('What is your tax category?\n'))
-    if taxCategory>5 or taxCategory<=0:
+    taxCategory=(input('What is your tax category?\n'))
+    if int(taxCategory)>5 or int(taxCategory)<=0:
         print('print enter a valid value')
         return calculateTaxCategory()
-    taxCategoryPercentages={1:2.8, 2:3.4, 3:4.8, 4:5.2, 5:6.1}
-    return taxCategoryPercentages[taxCategory]
+    taxCategoryPercentages={'1':2.8, '2':3.4, '3':4.8, '4':5.2, '5':6.1}
+
+    return (taxCategoryPercentages.get(taxCategory))
+    
 
 def calculateTax():
     netto=((100 - (calculateTaxCategory() - calculateChildTaxDiscount() ))/100 * inputGross())
     return netto
 
 netto=calculateTax()
-print('Your yearly net salary is', int(netto))
+print('Your yearly net salary is', (netto))
